f42dc2a37f8139f26b0b7c43bb0250e57f88715b
hsexton1214 <dancer1214@gmail.com>
2015-09-23 19:53:27 -0400

    made tower appear

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 var stage;
 var renderer;
+var tower;
 
 window.onload = function(){
     renderer = PIXI.autoDetectRenderer(1000, 600,{backgroundColor : 0xEEEEEE});
     document.getElementById('game_wrapper').appendChild(renderer.view);
     // create the root of the scene graph
     stage = new PIXI.Container();
+	tower = PIXI.Sprite.fromImage('images/tower.png');
+    tower.position.x=800;
+    tower.position.y=300;
+    stage.addChild(tower);
     // start animating
     animate();
 }
