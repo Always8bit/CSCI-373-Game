5e8f5663d389ed891b6162797eecb1335d2e8bf6
cable1985 <scable@unca.edu>
2015-09-24 11:37:36 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -147,9 +147,70 @@ window.onload = function(){
     init_robot();
     init_equationbox();
     init_numberLine();
+    target(800,100);
     // start animating
     animate();
 
+}
+
+function target(x,y){
+var target = PIXI.Sprite.fromImage('images/target.png');
+    
+    target.interactive = true;
+    target.buttonMode = true;
+    target.anchor.set(0.5);
+    target
+        // events for drag start
+        .on('mousedown', onDragStart)
+        .on('touchstart', onDragStart)
+        // events for drag end
+        .on('mouseup', onDragEnd)
+        .on('mouseupoutside', onDragEnd)
+        .on('touchend', onDragEnd)
+        .on('touchendoutside', onDragEnd)
+        // events for drag move
+        .on('mousemove', onDragMove)
+        .on('touchmove', onDragMove);
+
+    // move the sprite to its designated position
+    
+    target.position.x = x;
+    target.position.y = y;
+    
+    stage.addChild(target);
+}
+
+requestAnimationFrame( animate );
+
+
+function onDragStart(event)
+{
+    // store a reference to the data
+    // the reason for this is because of multitouch
+    // we want to track the movement of this particular touch
+    this.data = event.data;
+    this.alpha = 0.5;
+    this.dragging = true;
+}
+
+function onDragEnd()
+{
+    this.alpha = 1;
+
+    this.dragging = false;
+
+    // set the interaction data to null
+    this.data = null;
+}
+
+function onDragMove()
+{
+    if (this.dragging)
+    {
+        var newPosition = this.data.getLocalPosition(this.parent);
+        this.position.x = newPosition.x;
+        this.position.y = newPosition.y;
+    }
 }
 
 
