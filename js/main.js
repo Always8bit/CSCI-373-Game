e74a301e552b1e11e5bf753f11aae3804010adb2
Joseph El-Khouri <joseph.elkhouri@gmail.com>
2015-09-24 00:11:38 -0400

    Equation Box refactoring

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ function numberLine_update() {
     for (var i = 0; i <= (problemGenerator.rangeTop-problemGenerator.rangeBottom); i++) {
         var range = (problemGenerator.rangeTop-problemGenerator.rangeBottom);
         numberLine.drawRect((i/range)*nl_w, 0, 1, 10);
-        var text = new PIXI.Text((i+problemGenerator.rangeBottom),{font : '12px Arial', fill : 0x000000, align : 'center'});
+        var text = new PIXI.Text((i+problemGenerator.rangeBottom),{font : '16px Arial', fill : 0x000000, align : 'center'});
         text.x = (i/range)*nl_w;
         text.y = 20;
         numberLine.addChild(text);
@@ -31,7 +31,7 @@ function numberLine_update() {
 function init_numberLine() {
     numberLine = new PIXI.Graphics();
     numberLine.x = 20;
-    numberLine.y = 540;
+    numberLine.y = 555;
     numberLine_update();
     stage.addChild(numberLine);
 }
@@ -105,13 +105,32 @@ function init_robot() {
     stage.addChild(robot);
 }
 
+function equationBox_update() {
+    equationBox.clear();
+    var eqb_w = 600;
+    var eqb_h = 100;
+    equationBox.beginFill(0x333333, 1.0);
+    equationBox.lineStyle(2, 0x000000, 1.0);
+    equationBox.drawRect(0, 0, eqb_w, eqb_h);
+    equationBox.beginFill(0xFFFFFF, 1.0);
+    equationBox.drawRect(10, 10, eqb_h-20, eqb_h-20);
+    equationBox.drawRect(150, 10, eqb_h-20, eqb_h-20);
+    var n1 = new PIXI.Text(problemGenerator.num1,{font : '28px Arial', fill : 0x000000, align : 'center'});
+    var n2 = new PIXI.Text(problemGenerator.num2,{font : '28px Arial', fill : 0x000000, align : 'center'});
+    n1.x = 38;
+    n1.y = 35;
+    n2.x = 38+144;
+    n2.y = 35;
+    equationBox.addChild(n1);
+    equationBox.addChild(n2);
+}
+
 function init_equationbox() {
-    equationBox = PIXI.Sprite.fromImage('images/EquationBox.png');
-	equationBox.position.x=100;
-	equationBox.position.y=50;
-	stage.addChild(equationBox);    
-    equationText = PIXI.Text(problemGenerator.num1 + " + " + problemGenerator.num2, 
-        {font: "24px Arial", fill: 0x000000});
+    equationBox = new PIXI.Graphics();
+    equationBox.x = 10;
+    equationBox.y = 10;
+    equationBox_update();
+    stage.addChild(equationBox);
 }
 
 window.onload = function(){
