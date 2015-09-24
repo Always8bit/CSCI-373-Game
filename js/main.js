fbb743d2c31f7ea83cddb1a6defcbb69b7005d43
Joseph El-Khouri <joseph.elkhouri@gmail.com>
2015-09-23 21:40:07 -0400

    Declutter init function
    seperation makes things prettier

--- js/main.js
+++ js/main.js
@@ -3,19 +3,28 @@ var renderer;
 var tower;
 var robot;
 
+function init_tower() {
+    tower = PIXI.Sprite.fromImage('images/tower.png');
+    tower.position.x=800;
+    tower.position.y=300;
+    stage.addChild(tower);
+}
+
+function init_robot() {
+    robot = PIXI.Sprite.fromImage('images/robot.png');
+    robot.position.x=-50;
+    robot.position.y=500;
+    stage.addChild(robot);
+}
+
 window.onload = function(){
     renderer = PIXI.autoDetectRenderer(1000, 600,{backgroundColor : 0xEEEEEE});
     document.getElementById('game_wrapper').appendChild(renderer.view);
     // create the root of the scene graph
     stage = new PIXI.Container();
-	tower = PIXI.Sprite.fromImage('images/tower.png');
-    tower.position.x=800;
-    tower.position.y=300;
-    stage.addChild(tower);
-	robot = PIXI.Sprite.fromImage('images/robot.png');
-	robot.position.x=-50;
-	robot.position.y=500;
-	stage.addChild(robot);
+    // init stage
+    init_tower();
+    init_robot;
     // start animating
     animate();
 }
