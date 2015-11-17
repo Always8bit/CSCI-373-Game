80dc9b994c1ab0133c122fa9fde810216d914685
bonesteelkat <kboneste@unca.edu>
2015-11-17 16:01:10 -0500

    swapped problemgenerator and initbackground

--- js/main.js
+++ js/main.js
@@ -457,15 +457,15 @@ function lose_screen(){
 
 
 function init_background() { 
-    background_b = PIXI.Sprite.fromImage('images/backgroundbackground.png');
+  //  background_b = PIXI.Sprite.fromImage('images/backgroundbackground.png');
     
-//    if (problemGenerator.difficulty == 0) {
-//        background_b = PIXI.Sprite.fromImage('images/backgroundbackground.png');
-//    } else if (problemGenerator.difficulty == 1) {
-//        background_b = PIXI.Sprite.fromImage('images/backgroundsky_day.png');
-//    } else {
-//        background_b = PIXI.Sprite.fromImage('images/backgroundsky_night.png');        
-//    }
+    if (problemGenerator.difficulty == 0) {
+        background_b = PIXI.Sprite.fromImage('images/backgroundbackground.png');
+    } else if (problemGenerator.difficulty == 1) {
+        background_b = PIXI.Sprite.fromImage('images/backgroundsky_day.png');
+    } else {
+        background_b = PIXI.Sprite.fromImage('images/backgroundsky_night.png');        
+    }
     
     background_f = PIXI.Sprite.fromImage('images/backgroundforeground.png');
     background_b.x = 0;
@@ -579,8 +579,8 @@ function beginning_of_game() {
 			.load();
     
     
-    init_background();
     init_problemGenerator();
+    init_background();
     problemGenerator.setDifficulty(0);
     problemGenerator.generateNewProblem();
     init_numberLine();
@@ -628,8 +628,8 @@ function init_clouds() {
 
 function game_button(){
     stage = new PIXI.Container();
-    init_background();
     init_problemGenerator();
+    init_background();
     problemGenerator.setDifficulty(0);
     problemGenerator.generateNewProblem();
     init_missileDown();
